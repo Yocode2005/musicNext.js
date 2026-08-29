54e2bb1cf61a4b985c04920f3bbfc5efb53dedf3
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-08-29 17:09:57 +0530

    learn how to made route

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      <h1>File ka nam page.tsx/route.ts hi rkhna hai , ye automatically router bn jati hai</h1>
+    </div>
+    </>
+  );
+}
