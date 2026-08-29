2193f86a06211aa497ef39b4b1f0b76a3e8cd379
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-08-29 17:10:16 +0530

    understanding the working of the nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <h2>universal Navbar.. ye wala layout sabhi pages ko serve hota hai</h2>
+        {children}</body>
     </html>
   );
 }
